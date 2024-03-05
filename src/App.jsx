97425b6458408b4c314e108e2da4daa4c0f6791d
Mego ~ <Megahed Yahya>
2024-03-05 02:43:07 +0200

    create signup and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import LoginCompanies from "./pages/Companies/LoginCompanies/LoginCompanies";
+import SignupCompanies from "./pages/Companies/SignupCompanies/SignupCompanies";
+import ForgetPassCompanies from './pages/Companies/ForgetPasswordCompanies/ForgetPassCompanies';
+import UpdatePassCompanies from './pages/Companies/UpdatePasswordCompanies/UpdatePassCompanies';
+import ForgetPassUser from "./pages/User/ForgetPasswordUser/ForgetPassUser";
+import LoginUser from "./pages/User/LoginUser/LoginUser";
+import SignupUser from "./pages/User/SignupUser/SignupUser";
+import UpdatePassUser from "./pages/User/UpdatePasswordUser/UpdatePassUser";
+import "@mantine/core/styles.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/ForgetPassCompanies" Component={ForgetPassCompanies} />
+        <Route path="/LoginCompanies" Component={LoginCompanies} />
+        <Route path="/SignupCompanies" Component={SignupCompanies} />
+        <Route path="/UpdatePassWordCompanies" Component={UpdatePassCompanies} />
+        <Route path="/ForgetPasswordUser" Component={ForgetPassUser} />
+        <Route path="/LoginUser" Component={LoginUser} />
+        <Route path="/SignupUser" Component={SignupUser} />
+        <Route path="/UpdatePasswordUser" Component={UpdatePassUser} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
